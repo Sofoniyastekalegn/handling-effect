db01e845af55d84c2298944ce44605bfb0dea4db
Sofoniyastekalegn <sofoniyastekalegn@gmail.com>
2024-10-12 13:49:06 +0300

    alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { i } from 'vite/dist/node/types.d-aGj9QkWt'
-// consider the follwing example code snipter 
-import classes from "./BlogPosts.module.css";
- async function fetchPosts() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const blogPosts = awai response.json();
-  return blogPosts;
+import Alert from './components/Alert';
 
- }
-
- function BlogPosts() {
-  const [loadedPosts, setLoadedPosts] = useState([]);
-  fetchPosts().then((fetchedPosts) => setLoadedPosts(fetchedPosts));
-  return (
-    <ul className={'classes.posts'}>
-      {loadedPosts.map((post) => {
-        <li key={post.id}>{post.title}</li>
-      })}
-    </ul>
-  );
- 
+function App() {
+  return <Alert />;
 }
 
-export default BlogPosts;
+export default App;
